54ff789695a300479e043a3ec8d75e29cbf46039
Damilare50 <damilarelamine@gmail.com>
2022-04-21 02:20:51 +0100

    footer files

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 //Components
 import About from "./components/About";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import Products from "./components/Products";
@@ -19,6 +20,7 @@ const App = () => {
         <Route path="/products" element={<Products />} />
         <Route path="*" element={<Home />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }

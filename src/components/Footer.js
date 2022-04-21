a586b9631cd902bc54e364aa473f88975792e42f
Damilare50 <damilarelamine@gmail.com>
2022-04-21 19:57:49 +0100

    complete navbar

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div><h1>Footer component</h1></div>
+    <div><h1>Footer</h1></div>
   )
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,47 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/NavBar.css';
+import RestaurantLogo from '../images/restaurant-logo.png';
+
+const Logo = ({logoImg}) => {
+  return (
+    <>
+      <Link to='/'>
+        <img src={logoImg} alt='logo' className='logo-img'/>
+      </Link>
+    </>
+  )
+}
+
+const Navlink = () => {
+  return (
+    <>
+      <ul className='nav-links'>
+        <li>
+          <Link to='/' className='link'>Home</Link>
+        </li>
+        <li>
+          <Link to='/about' className='link'>About</Link>
+        </li>
+        <li>
+          <Link to='/contact' className='link'>Contact</Link>
+        </li>
+        <li>
+          <Link to='/products' className='link'>Products</Link>
+        </li>
+      </ul>
+    </>
+  )
+}
 
 const NavBar = () => {
   return (
-    <div><h1>NavBar</h1></div>
+    <>
+      <nav className='navbar'>
+        <Logo logoImg={RestaurantLogo} />
+        <Navlink />
+      </nav>
+    </>
   )
 }
 

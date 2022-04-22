9a57b9dc74cd72edc150d5b77b2358dc6bf11abd
Damilare50 <damilarelamine@gmail.com>
2022-04-22 03:05:53 +0100

    complete footer section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,70 @@
 import React from 'react';
+import '../styles/Footer.css';
+import { FaTwitter, FaFacebookF, FaInstagram, FaSkype, FaLinkedinIn } from 'react-icons/fa'
+import { Link } from 'react-router-dom';
+
+const Address = () => {
+  return (
+    <>
+      <article>
+        <h2>FOODIELAND</h2>
+        <section>
+          <h4>13, Macaulay Way, <br/>LG, 200142, NGA</h4>
+        </section>
+        <section>
+          <h4>Phone: +234 901 234 4567</h4>
+          <h4>Email: info@group55.com</h4>
+        </section>
+        <section>
+          <FaTwitter className='social' />
+          <FaFacebookF className='social' />
+          <FaInstagram className='social' />
+          <FaSkype className='social' />
+          <FaLinkedinIn className='social' />
+        </section>
+      </article>
+    </>
+  )
+}
+
+const UsefulLinks = () => {
+  return (
+    <article>
+      <h2>Useful Links</h2>
+      <ul>
+        <li><Link to="/" className='footer-link'>Home</Link></li>
+        <li><Link to="/about" className='footer-link'>About us</Link></li>
+        <li><Link to="/products" className='footer-link'>Services</Link></li>
+        <li><Link to="/" className='footer-link'>Terms of service</Link></li>
+        <li><Link to="/" className='footer-link'>Privacy policy</Link></li>
+      </ul>
+    </article>
+  )
+}
+
+const Newsletter = () => {
+  return (
+    <>
+      <article>
+        <h2>Our Newsletter</h2>
+        <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
+        <section className='news-form'>
+          <input type="text" name="newsletter" id="" placeholder='Enter your mail...'/>
+          <button type='submit'>Subscribe</button>
+        </section>
+      </article>
+    </>
+  )
+}
 
 const Footer = () => {
   return (
-    <div><h1>Footer</h1></div>
+    <footer id='footer'>
+      <Address />
+      <UsefulLinks />
+      <UsefulLinks />
+      <Newsletter />
+    </footer>
   )
 }
 
